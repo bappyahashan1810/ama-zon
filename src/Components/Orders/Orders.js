4d718edba48c1e01b09bb4feeb481349e3798952
bappy-ahashan <bappyahashan@gmail.com>
2023-07-12 15:30:05 +0600

    added button to remove items and set handler and call removeitem function from localstorage

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -2,17 +2,23 @@ import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
     const { products, previousCart } = useLoaderData();
     const [cart, setCart] = useState(previousCart);
-    console.log(previousCart);
+    const handleRemoveItem = (id) => {
+        const remainingCart = cart.filter(product => product.id !== id);
+        setCart(remainingCart);
+        removeFromDb(id);
+    }
     return (
         <div className='product-order-container' >
             <div className="orders-container">
                 {
                     cart.map(product => <ReviewItem
                         key={product.id}
+                        handleRemoveItem={handleRemoveItem}
                         product={product}
                     ></ReviewItem>)
                 }
